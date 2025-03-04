ff8e6b4921288888b3b22c4fc44ae7aeeb4e22bc
archanVadgama <archan.vadgama@simformsolutions.com>
2025-03-04 10:55:56 +0530

    UPD: Refactor code

--- dist/Modules/ThemeMode.js
+++ dist/Modules/ThemeMode.js
@@ -1,28 +1,20 @@
 import { showToast } from "./Toast.js";
 // Function to initialize the theme based on local storage
 export function initializeTheme() {
+    var _a;
     let toggleMode = document.querySelector(".toggle-mode");
     let calculator = document.getElementsByClassName("calculator")[0];
     // Check the saved theme mode from localStorage or default to light mode
     const themeMode = localStorage.getItem("theme-mode") === "dark-mode"
         ? "dark-mode"
         : "light-mode";
-    // Set the initial theme based on the saved mode
-    if (themeMode === "dark-mode") {
-        toggleMode.innerHTML = '<i class="fa-solid fa-sun"></i>'; // sun icon for dark mode
-        document.body.style.backgroundColor = "#1b1c1d";
-        calculator.style.backgroundColor = "#1b1c1d";
-    }
-    else {
-        document.body.style.backgroundColor = "#f4f4f4";
-        calculator.style.backgroundColor = "#fff";
-        toggleMode.innerHTML = '<i class="fa-solid fa-moon"></i>'; // moon icon for light mode
-    }
+    // Set the theme mode
+    setThemeMode(themeMode, toggleMode, calculator);
     // Ensure the element exists before adding event listeners
     if (toggleMode) {
         // Apply the saved theme
-        const savedTheme = localStorage.getItem("theme-mode");
-        if (savedTheme && savedTheme === "dark-mode") {
+        const savedTheme = (_a = localStorage.getItem("theme-mode")) !== null && _a !== void 0 ? _a : "light-mode";
+        if (savedTheme === "dark-mode") {
             document.body.classList.add("dark-mode");
         }
         else {
@@ -33,23 +25,34 @@ export function initializeTheme() {
             const themeMode = document.body.classList.contains("dark-mode")
                 ? "light-mode"
                 : "dark-mode";
-            // Change the UI for dark or light mode
-            if (themeMode === "dark-mode") {
-                this.innerHTML = '<i class="fa-solid fa-sun"></i>'; // sun icon for dark mode
-                document.body.style.backgroundColor = "#1b1c1d";
-                calculator.style.backgroundColor = "#1b1c1d";
-            }
-            else {
-                this.innerHTML = '<i class="fa-solid fa-moon"></i>'; // moon icon for light mode
-                document.body.style.backgroundColor = "#f4f4f4";
-                calculator.style.backgroundColor = "#fff";
-            }
+            // Set the theme mode
+            setThemeMode(themeMode, toggleMode, calculator);
             // Call showToast function if necessary (ensure showToast is available)
             showToast(`Change to ${themeMode}`);
             // Toggle class and update localStorage
             document.body.classList.toggle("dark-mode");
             localStorage.setItem("theme-mode", themeMode);
         });
+    }
+}
+/**
+ * Set the theme mode
+ *
+ * @param {string} themeMode
+ * @param {HTMLElement} buttonElement
+ * @param {HTMLElement} calculator
+ */
+function setThemeMode(themeMode, buttonElement, calculator) {
+    // Change the UI for dark or light mode
+    if (themeMode === "dark-mode") {
+        buttonElement.innerHTML = '<i class="fa-solid fa-sun"></i>'; // sun icon for dark mode
+        document.body.style.backgroundColor = "#1b1c1d";
+        calculator.style.backgroundColor = "#1b1c1d";
+    }
+    else {
+        buttonElement.innerHTML = '<i class="fa-solid fa-moon"></i>'; // moon icon for light mode
+        document.body.style.backgroundColor = "#f4f4f4";
+        calculator.style.backgroundColor = "#fff";
     }
 }
 // Function to open the history popup
